61051144e04dbaccd1cbd78d4d647cd303e61b36
Patrick <1481463@alu.murciaeduca.es>
2024-11-21 09:06:27 +0100

    Mover rutas

--- routes/curriculos.php
+++ routes/curriculos.php
@@ -1,1 +1,7 @@
 <?php
+
+
+Route::get('curriculos', [CurriculoController::class, 'getIndex']);
+Route::get('curriculos/show/{id}', [CurriculoController::class, 'getShow']);
+Route::get('curriculos/create', [CurriculoController::class, 'getCreate']);
+Route::get('curriculos/edit/{id}', [CurriculoController::class, 'getEdit']);
