934f8868bc7f5286283736035fea7629720b6181
Patrick <1481463@alu.murciaeduca.es>
2024-11-26 11:24:49 +0100

    Pruebas con middleware

---
+++ app/Http/Middleware/MyMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class MyMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next, $param1): Response
+    {
+        // Perform action
+            if($request->route()->hasParameter('id') && $request->route()->parameter('id') >$param1){
+                return redirect('/'); // Redirect to home page
+            }
+        return  $next($request);
+    }
+}
