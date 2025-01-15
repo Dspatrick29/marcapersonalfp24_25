d62fd129b2b4ae753f9385f81cbd33b7b434ed01
Dspatrick29 <example@example.com>
2025-01-15 09:01:03 +0100

    Controlador de recursos de Proyectos terminado

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\CicloController;
+use App\Http\Controllers\API\ProyectoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Psr\Http\Message\ServerRequestInterface;
@@ -14,6 +15,11 @@ Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
 Route::prefix('v1')->group(function () {
     Route::apiResource('ciclos', CicloController::class);
 });
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('proyectos', ProyectoController::class);
+});
+
 
 Route::any('/{any}', function (ServerRequestInterface $request) {
     $config = new Config([
